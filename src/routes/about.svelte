87cd3c05044c48dca9cba905722cb39ebc92b8bb
MIchael Berry <michaeladamberry3@gmail.com>
2020-02-01 10:13:40 -0500

    link to pdf of updated resume

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -32,7 +32,12 @@
       on Unsplash
     </figcaption> 
   </figure> -->
+  <p>
+    View my
+    <a href="adam_berry_pdf.pdf">resume (pdf)</a>
+  </p>
   <div class="svg-container">
+
     <svg
       xmlns="http://www.w3.org/2000/svg"
       xmlns:xlink="http://www.w3.org/1999/xlink"
