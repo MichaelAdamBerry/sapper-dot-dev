56df1aa9de2f67f1c4655955bcb8c338180f5905
MIchael Berry <michaeladamberry3@gmail.com>
2020-02-05 08:33:42 -0500

    updated resume

--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -13,7 +13,7 @@
 <div>
   <iframe
     title="pdf of my current resume"
-    src="https://res.cloudinary.com/dscjol9s7/image/upload/v1580572633/personal-website/adam_berry_pdf_hksxec.pdf"
+    src="https://res.cloudinary.com/dscjol9s7/image/upload/v1580908693/personal-website/resume-michael-adam-berry_1_kcs6ei.pdf"
     width="100%"
     height="100%" />
 </div>
